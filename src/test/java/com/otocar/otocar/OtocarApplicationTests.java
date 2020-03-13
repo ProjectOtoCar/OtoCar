dc7b1b95a88124adf94fffd1ea452300d6ea93e9
Kamil Nowak <kamilnowakx98@gmail.com>
2020-03-13 13:09:42 +0100

    init project

---
+++ src/test/java/com/otocar/otocar/OtocarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otocar.otocar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtocarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
