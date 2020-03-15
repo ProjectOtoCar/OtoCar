68c5b02bbf3dbb8fc7cd48359b56efb6d3dc9d95
Kamil Nowak <kamilnowakx98@gmail.com>
2020-03-15 11:46:23 +0100

    add Image model

---
+++ src/main/java/com/otocar/otocar/model/Image.java
@@ -0,0 +1,58 @@
+package com.otocar.otocar.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private byte[] photo;
+    private boolean isMainImage;
+
+    @ManyToOne
+    private Advertisement advertisement;
+
+    public Image() {
+    }
+
+    public Image(byte[] photo, boolean isMainImage, Advertisement advertisement) {
+        this.photo = photo;
+        this.isMainImage = isMainImage;
+        this.advertisement = advertisement;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public boolean isMainImage() {
+        return isMainImage;
+    }
+
+    public void setMainImage(boolean mainImage) {
+        isMainImage = mainImage;
+    }
+
+    public Advertisement getAdvertisement() {
+        return advertisement;
+    }
+
+    public void setAdvertisement(Advertisement advertisement) {
+        this.advertisement = advertisement;
+    }
+}
