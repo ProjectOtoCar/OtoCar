be984df5eb46c3aef3751281c5e62aca5f3eb694
Kamil Nowak <kamilnowakx98@gmail.com>
2020-03-16 13:43:27 +0100

    add query

--- src/main/java/com/otocar/otocar/repository/AdvertisementRepository.java
+++ src/main/java/com/otocar/otocar/repository/AdvertisementRepository.java
@@ -2,9 +2,19 @@ package com.otocar.otocar.repository;
 
 import com.otocar.otocar.model.Advertisement;
 import com.otocar.otocar.model.Image;
+import com.otocar.otocar.model.Seller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface AdvertisementRepository  extends PagingAndSortingRepository<Advertisement, Long> {
+    Page<Advertisement> findAllByActive(boolean active,Pageable pageable);
+    Page<Advertisement> findAllByActiveAndDateAddGreaterThan(boolean active, LocalDate dateAdd, Pageable pageable);
+    Page<Advertisement> findAllByActiveAndCity(boolean active, String city,Pageable pageable);
+    Page<Advertisement> findAllByActiveAndDateAddGreaterThanAndCity(boolean active, LocalDate dateAdd, String city,Pageable pageable);
+    Page<Advertisement> findAllBySellerAndActive(Seller seller, boolean active, Pageable pageable);
 }
