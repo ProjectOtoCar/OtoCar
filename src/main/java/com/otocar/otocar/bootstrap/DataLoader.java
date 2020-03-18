0614b403efc1c395bf36befeb83479b7274d1357
Mariusz Kaczmarek <mariuszkaczmrek97@outlook.com>
2020-03-18 13:15:19 +0100

    fixed pagable

--- src/main/java/com/otocar/otocar/bootstrap/DataLoader.java
+++ src/main/java/com/otocar/otocar/bootstrap/DataLoader.java
@@ -59,15 +59,17 @@ public class DataLoader implements CommandLineRunner {
         Car car2 = new Car(brandList.get(2), 1000, 1000,modelList.get(3), TypeFuel.DIESEL, 2020, 400_000, TypeCar.KABRIOLET, Color.POMARANCZOWY);
         Advertisement advertisement = new Advertisement(BigDecimal.valueOf(10000), LocalDate.now(), true, "title", "content", cityList.get(0), car, seller , null );
         Advertisement advertisement1 = new Advertisement(BigDecimal.valueOf(1324300), LocalDate.now(), true, "title23", "content321", cityList.get(3), car1, seller , null );
-        Advertisement advertisement2 = new Advertisement(BigDecimal.valueOf(14230), LocalDate.now(), false, "title1", "co213ntent312321", cityList.get(10), car2, seller , null );
+        Advertisement advertisement2 = new Advertisement(BigDecimal.valueOf(14230), LocalDate.now(), true, "title1", "co213ntent312321", cityList.get(10), car2, seller , null );
+        Advertisement advertisement6 = new Advertisement(BigDecimal.valueOf(15484121),LocalDate.of(2020,03,19),true,"testy","testCont",cityList.get(5),car1,seller,null);
         seller.getAdvertisement().add(advertisement);
         seller.getAdvertisement().add(advertisement1);
         seller.getAdvertisement().add(advertisement2);
+        seller.getAdvertisement().add(advertisement6);
         sellerService.save(seller);
 
         Seller seller1 = new Seller("123456", "123456789", TypeAccount.ADMIN, "111111111", LocalDate.now(), LocalDate.now(), new HashSet<Advertisement>(),LocalDate.now());
         Car car3 = new Car(brandList.get(10), 10, 100, modelList.get(4), TypeFuel.PETROL, 1990, 20_000, TypeCar.SEDAN, Color.ZOLTY);
-        Advertisement advertisement3 = new Advertisement(BigDecimal.valueOf(100), LocalDate.now(), false, "123456", "kjhgfdsa", cityList.get(7), car3, seller, null);
+        Advertisement advertisement3 = new Advertisement(BigDecimal.valueOf(100), LocalDate.now(), true, "123456", "kjhgfdsa", cityList.get(7), car3, seller, null);
         seller1.getAdvertisement().add(advertisement3);
 
         sellerService.save(seller1);
