6e352dbdcaf3e33c79638f16a70dd493a89d0420
Kamil Nowak <kamilnowakx98@gmail.com>
2020-03-20 17:19:34 +0100

    add class SellerServicesTest

---
+++ src/test/java/com/otocar/otocar/service/SellerServiceTest.java
@@ -0,0 +1,45 @@
+package com.otocar.otocar.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SellerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void testFindAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void change() {
+    }
+
+    @Test
+    void patch() {
+    }
+
+    @Test
+    void findAllByNameAndTypeAndPremium() {
+    }
+}
