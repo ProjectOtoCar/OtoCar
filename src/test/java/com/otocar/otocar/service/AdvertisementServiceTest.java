0034b71a6947cd13569aeb2538cf02f395bfecbe
Mariusz Kaczmarek <mariuszkaczmrek97@outlook.com>
2020-03-24 16:42:32 +0100

    Add advertismen tests

--- src/test/java/com/otocar/otocar/service/AdvertisementServiceTest.java
+++ src/test/java/com/otocar/otocar/service/AdvertisementServiceTest.java
@@ -6,6 +6,7 @@ import com.otocar.otocar.repository.AdvertisementRepository;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,6 +15,10 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -156,7 +161,34 @@ class AdvertisementServiceTest {
     }
 
     @Test
-    void findAllAdvertisementBySeller() {
+    void should_find_All_Advertisement_By_Seller() {
+        ArrayList<Advertisement> advArrayList = new ArrayList<>();
+        Advertisement adv = new Advertisement();
+        adv.setId(1L);
+        Advertisement adv1 = new Advertisement();
+        adv1.setId(2L);
+        advArrayList.add(adv);
+        advArrayList.add(adv1);
+
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+
+        given(advertisementRepository.findAllAdvertisementBySeller(1L, pageable)).willReturn(advPage);
+
+        Iterable<Advertisement> foundAdvSellers = advertisementService.findAllAdvertisementBySeller(1L,1);
+
+        assertThat(foundAdvSellers).isNotNull();
+
+        List<Advertisement> foundAdVSell = new ArrayList<>();
+        foundAdvSellers.forEach(foundAdVSell::add);
+
+        then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+        assertThat(foundAdVSell.size()).isEqualTo(advPage.getTotalElements());
+
+        assertThat(foundAdVSell.get(0).getId()).isEqualTo(1L);
+        assertThat(foundAdVSell.get(1).getId()).isEqualTo(2L);
+
     }
 
     @Test
@@ -185,5 +217,174 @@ class AdvertisementServiceTest {
         assertThat(foundAdv).isNull();
     }
 
+    @Nested
+    class FilterAdv{
+        ArrayList<Advertisement> advArrayList;
+        Pageable pageable;
+        BigDecimal price = BigDecimal.valueOf(10_000);
+        LocalDate data = LocalDate.now();
+        boolean isActive = true;
+        String title = "Test";
+        String content = "TestTestTestContentTest";
+        Long id = 1L;
+        Advertisement adv = new Advertisement(price, data, isActive, title, content, null, null, null, null);
+        Advertisement adv1 = new Advertisement(price, data, isActive, title, content, null, null, null, null);
+        Advertisement adv2 = new Advertisement(price, data, isActive, title, content, null, null, null, null);
+
+
+        @BeforeEach
+        public void setUp(){
+            adv.setId(1L);
+            adv1.setId(2L);
+            adv2.setId(3L);
+            pageable = PageRequest.of(0, 10);
+            advArrayList = new ArrayList<>();
+            advArrayList.add(adv);
+            advArrayList.add(adv1);
+            advArrayList.add(adv2);
+
+        }
+
+
+        @Test
+        public void  should_Find_Firstregistration_Beetwen_Price_Beetwer_Order_By_Asc(){
+            Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+            given(advertisementRepository.findAllByActiveAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceAsc(true,pageable,1990,2000,price,price)).willReturn(advPage);
+
+            Iterable<Advertisement> advertisements = advertisementService.find(1, null, null, 1990, 2000, price, price, "asc");
+
+            assertThat(advertisements).isNotNull();
+
+            List<Advertisement> foundAdv = new ArrayList<>();
+
+            advertisements.forEach(foundAdv::add);
+
+            then(advertisementRepository).should().findAllByActiveAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceAsc(true,pageable,1990,2000,price,price);
+            then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+            assertThat(foundAdv.size()).isEqualTo(advPage.getTotalElements());
+
+            assertThat(foundAdv.get(0).getId()).isEqualTo(1L);
+            assertThat(foundAdv.get(1).getId()).isEqualTo(2L);
+            assertThat(foundAdv.get(2).getId()).isEqualTo(3L);
+
+        }
+        @Test
+        public void  should_Find_Firstregistration_Beetwen_Price_Beetwer_Order_By_Desc(){
+            Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+            given(advertisementRepository.findAllByActiveAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceDesc(true,pageable,1990,2000,price,price)).willReturn(advPage);
+
+            Iterable<Advertisement> advertisements = advertisementService.find(1, null, null, 1990, 2000, price, price, "desc");
+
+            assertThat(advertisements).isNotNull();
+
+            List<Advertisement> foundAdv = new ArrayList<>();
+
+            advertisements.forEach(foundAdv::add);
+
+            then(advertisementRepository).should().findAllByActiveAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceDesc(true,pageable,1990,2000,price,price);
+            then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+            assertThat(foundAdv.size()).isEqualTo(advPage.getTotalElements());
+
+            assertThat(foundAdv.get(0).getId()).isEqualTo(1L);
+            assertThat(foundAdv.get(1).getId()).isEqualTo(2L);
+            assertThat(foundAdv.get(2).getId()).isEqualTo(3L);
+
+        }
+
+        @Test
+        public void  should_Find_BrandName_Firstregistration_Beetwen_Price_Beetwer_Order_By_Asc(){
+            Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+            given(advertisementRepository.findAllByActiveAndCar_Brand_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceAsc(true,pageable,"BMW",1990,2000,price,price)).willReturn(advPage);
+
+            Iterable<Advertisement> advertisements = advertisementService.find(1, "BMW", null, 1990, 2000, price, price, "asc");
+
+            assertThat(advertisements).isNotNull();
+
+            List<Advertisement> foundAdv = new ArrayList<>();
+
+            advertisements.forEach(foundAdv::add);
+
+            then(advertisementRepository).should().findAllByActiveAndCar_Brand_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceAsc(true,pageable,"BMW",1990,2000,price,price);
+            then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+            assertThat(foundAdv.size()).isEqualTo(advPage.getTotalElements());
+
+            assertThat(foundAdv.get(0).getId()).isEqualTo(1L);
+            assertThat(foundAdv.get(1).getId()).isEqualTo(2L);
+            assertThat(foundAdv.get(2).getId()).isEqualTo(3L);
+
+        }
+
+        @Test
+        public void  should_Find_BrandName_Firstregistration_Beetwen_Price_Beetwer_Order_By_Desc(){
+            Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+            given(advertisementRepository.findAllByActiveAndCar_Brand_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceDesc(true,pageable,"BMW",1990,2000,price,price)).willReturn(advPage);
+
+            Iterable<Advertisement> advertisements = advertisementService.find(1, "BMW", null, 1990, 2000, price, price, "desc");
+
+            assertThat(advertisements).isNotNull();
+
+            List<Advertisement> foundAdv = new ArrayList<>();
+
+            advertisements.forEach(foundAdv::add);
+
+            then(advertisementRepository).should().findAllByActiveAndCar_Brand_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceDesc(true,pageable,"BMW",1990,2000,price,price);
+            then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+            assertThat(foundAdv.size()).isEqualTo(advPage.getTotalElements());
+
+            assertThat(foundAdv.get(0).getId()).isEqualTo(1L);
+            assertThat(foundAdv.get(1).getId()).isEqualTo(2L);
+            assertThat(foundAdv.get(2).getId()).isEqualTo(3L);
+        }
+
+        @Test
+        public void  should_Find_BrandName_Model_Name_Firstregistration_Beetwen_Price_Beetwer_Order_By_Desc(){
+            Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+            given(advertisementRepository.findAllByActiveAndCar_Brand_NameAndCar_Model_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceDesc(true,pageable,"BMW","M3",1990,2000,price,price)).willReturn(advPage);
+
+            Iterable<Advertisement> advertisements = advertisementService.find(1, "BMW", "M3", 1990, 2000, price, price, "desc");
+
+            assertThat(advertisements).isNotNull();
+
+            List<Advertisement> foundAdv = new ArrayList<>();
+
+            advertisements.forEach(foundAdv::add);
+
+            then(advertisementRepository).should().findAllByActiveAndCar_Brand_NameAndCar_Model_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceDesc(true,pageable,"BMW","M3",1990,2000,price,price);
+            then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+            assertThat(foundAdv.size()).isEqualTo(advPage.getTotalElements());
+
+            assertThat(foundAdv.get(0).getId()).isEqualTo(1L);
+            assertThat(foundAdv.get(1).getId()).isEqualTo(2L);
+            assertThat(foundAdv.get(2).getId()).isEqualTo(3L);
+        }
+
+        @Test
+        public void  should_Find_BrandName_Model_Name_Firstregistration_Beetwen_Price_Beetwer_Order_By_Asc(){
+            Page<Advertisement> advPage = new PageImpl<>(advArrayList,pageable,advArrayList.size());
+            given(advertisementRepository.findAllByActiveAndCar_Brand_NameAndCar_Model_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceAsc(true,pageable,"BMW","M3",1990,2000,price,price)).willReturn(advPage);
+
+            Iterable<Advertisement> advertisements = advertisementService.find(1, "BMW", "M3", 1990, 2000, price, price, "asc");
+
+            assertThat(advertisements).isNotNull();
+
+            List<Advertisement> foundAdv = new ArrayList<>();
+
+            advertisements.forEach(foundAdv::add);
+
+            then(advertisementRepository).should().findAllByActiveAndCar_Brand_NameAndCar_Model_NameAndCar_FirstRegistrationBetweenAndPriceBetweenOrderByPriceAsc(true,pageable,"BMW","M3",1990,2000,price,price);
+            then(advertisementRepository).shouldHaveNoMoreInteractions();
+
+            assertThat(foundAdv.size()).isEqualTo(advPage.getTotalElements());
+
+            assertThat(foundAdv.get(0).getId()).isEqualTo(1L);
+            assertThat(foundAdv.get(1).getId()).isEqualTo(2L);
+            assertThat(foundAdv.get(2).getId()).isEqualTo(3L);
+        }
+    }
 
 }
