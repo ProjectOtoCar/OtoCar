0f6a3da29384c556dd91418b40a00551009fc700
Mariusz Kaczmarek <mariuszkaczmrek97@outlook.com>
2020-03-31 11:08:11 +0200

    Add corsOrginMapping

--- src/main/java/com/otocar/otocar/OtocarApplication.java
+++ src/main/java/com/otocar/otocar/OtocarApplication.java
@@ -2,12 +2,27 @@ package com.otocar.otocar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OtocarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OtocarApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsCofigure(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("api/**")
+                    .allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 
 }
