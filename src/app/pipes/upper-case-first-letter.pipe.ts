0e0a38444c2c60a2bc2acde70191720be983c532
Kamil Nowak <kamilnowakx98@gmail.com>
2020-04-03 15:11:26 +0200

    fix problem with pipe upperCaseFirstLetter

--- src/app/pipes/upper-case-first-letter.pipe.ts
+++ src/app/pipes/upper-case-first-letter.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class UpperCaseFirstLetterPipe implements PipeTransform {
 
   transform(value: string): string {
+    if(value === null || value === undefined) {
+      return value;
+    }
     return value[0].toUpperCase() + value.substr(1, value.length);
   }
 
