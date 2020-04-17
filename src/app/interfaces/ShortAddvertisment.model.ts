52feb6f611b52cdccaabef761a2acbcde20e50e2
Kamil Nowak <kamilnowakx98@gmail.com>
2020-04-17 12:50:50 +0200

    add style to UserAddvertismentComponent

---
+++ src/app/interfaces/ShortAddvertisment.model.ts
@@ -0,0 +1,21 @@
+import { Image } from './Image';
+
+export interface ShortAddvertisment {
+    id: number;
+    price: number;
+    dataAdd: Date;
+    active: boolean;
+    title: string;
+    content: string;
+    car: {
+        id: number;
+        fuel: string;
+        mileage: number;
+        firstRegistartion: number;
+    };
+    seller: {
+        id: number;
+        premiumAccount: Date;
+    };
+    images: [Image];
+}
--- src/app/user-page/user-details/user-addvertisments/user-addvertisment-item/user-addvertisment-item.component.ts
+++ src/app/user-page/user-details/user-addvertisments/user-addvertisment-item/user-addvertisment-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ShortAddvertisment } from 'src/app/interfaces/ShortAddvertisment.model';
 
 @Component({
   selector: 'app-user-addvertisment-item',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAddvertismentItemComponent implements OnInit {
 
+  @Input() shortAdvertisment: ShortAddvertisment;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showDetails(): void {
+
+  }
+
+  modifyAddvertisment(): void {
+
+  }
+
+  deleteAddvertisment(): void {
+
+  }
+
+  toggleActive(): void {
+    
   }
 
 }
