67e8332515498c8fd1fc8d23ccce991fe0013645
Kamil Nowak <kamilnowakx98@gmail.com>
2020-04-19 01:33:24 +0200

    add ErrorMessageComponent

---
+++ src/app/error-message/error-message.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.scss']
+})
+export class ErrorMessageComponent implements OnInit {
+  @Input() errorMessage = 'Coś poszło nie tak';
+  @Input() isLink = false;
+  @Input() url: string[];
+  @Input() queryParams;
+  @Input() linkMessage;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
