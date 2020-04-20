1d9d764f499e5edc52f55878b86f11b23cfe1b0a
Kamil Nowak <kamilnowakx98@gmail.com>
2020-04-20 02:14:31 +0200

    add SuccessMessageComponent

---
+++ src/app/success-message/success-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-success-message',
+  templateUrl: './success-message.component.html',
+  styleUrls: ['./success-message.component.scss']
+})
+export class SuccessMessageComponent implements OnInit {
+  @Input() successMessage = 'Sukces!';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
