29de7ba739961038e34f9a88cd9d5d96276cc7e0
Kamil Nowak <kamilnowakx98@gmail.com>
2020-06-02 11:58:16 +0200

    added product url to enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  basicUrl: 'http://localhost:8080',
-  loginUrl: 'http://localhost:9090',
+  basicUrl: 'http://5.152.161.49:8080',
+  loginUrl: 'http://5.152.161.49:9090',
   contactEmail: 'ToJestMailDoProjektuOtoCar@gmail.com'
 };
